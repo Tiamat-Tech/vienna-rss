d312e17237b596c94b25181c248c7675aad04027
Barijaona Ramaholimihaso <github.com@barijaona.com>
2026-09-03 04:05:19 +0200

    Complete port of ArticleListView changes

--- Vienna/Sources/Main window/UnifiedDisplayView.m
+++ Vienna/Sources/Main window/UnifiedDisplayView.m
@@ -594,71 +594,33 @@ static void *VNAUnifiedDisplayViewObserverContext = &VNAUnifiedDisplayViewObserv
  */
 -(BOOL)copyTableSelection:(NSIndexSet *)rowIndexes toPasteboard:(NSPasteboard *)pboard
 {
-	NSMutableArray * arrayOfURLs = [[NSMutableArray alloc] init];
-	NSMutableArray * arrayOfTitles = [[NSMutableArray alloc] init];
-	NSMutableString * fullHTMLText = [[NSMutableString alloc] init];
-	NSMutableString * fullPlainText = [[NSMutableString alloc] init];
 	NSInteger count = rowIndexes.count;
+	NSMutableArray * pbItems = [NSMutableArray arrayWithCapacity:count];
 
 	// Set up the pasteboard
-	[pboard declareTypes:@[VNAPasteboardTypeWebURLsWithTitles, NSPasteboardTypeString, NSPasteboardTypeHTML] owner:self];
-    if (count == 1) {
-        [pboard addTypes:@[NSPasteboardTypeURL, VNAPasteboardTypeURLName]
-                   owner:self];
-    }
+	[pboard prepareForNewContentsWithOptions:NSPasteboardContentsCurrentHostOnly];
 
-	// Open the HTML string
-	[fullHTMLText appendString:@"<html style=\"font-family:sans-serif;\">"
-                                "<head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"></head><body>"];
-
+	NSInteger countOfItems = 0;
 	// Get all the articles that are being dragged
 	NSUInteger msgIndex = rowIndexes.firstIndex;
 	while (msgIndex != NSNotFound) {
-		Article * thisArticle = self.articleController.allArticles[msgIndex];
-		NSString * msgText = thisArticle.body;
-		NSString * msgTitle = thisArticle.title;
-		NSString * msgLink = thisArticle.link;
-
-		[arrayOfURLs addObject:msgLink];
-		[arrayOfTitles addObject:msgTitle];
-
-		// Plain text
-        [fullPlainText appendFormat:@"%@\n%@\n\n", msgTitle, thisArticle.summary];
-
-		// Add HTML version too.
-		[fullHTMLText appendFormat:@"<div class=\"info\"><a href=\"%@\">%@</a><div>"
-                                    "<div class=\"articleBodyStyle\">%@</div><br>", msgLink, msgTitle, msgText];
-
-		if (count == 1) {
-			[pboard setString:msgLink forType:NSPasteboardTypeURL];
-			[pboard setString:msgTitle forType:VNAPasteboardTypeURLName];
-
-			// Write the link to the pastboard.
-			[[NSURL URLWithString:msgLink] writeToPasteboard:pboard];
+		NSPasteboardItem *pboardItem = (NSPasteboardItem *)[self pasteboardWriterForIndex:msgIndex];
+		if (pboardItem.types) {
+			[pbItems addObject:pboardItem];
+		    ++countOfItems;
 		}
 
-		//increment
-    	msgIndex = [rowIndexes indexGreaterThanIndex: msgIndex];
+		msgIndex = [rowIndexes indexGreaterThanIndex:msgIndex];
 	}
 
-	// Close the HTML string
-	[fullHTMLText appendString:@"</body></html>"];
-
-	// Put string on the pasteboard for external drops.
-	[pboard setPropertyList:@[arrayOfURLs, arrayOfTitles] forType:VNAPasteboardTypeWebURLsWithTitles];
-	[pboard setString:fullPlainText forType:NSPasteboardTypeString];
-	[pboard setString:fullHTMLText.vna_stringByEscapingExtendedCharacters forType:NSPasteboardTypeHTML];
-
-	return YES;
+	[pboard writeObjects:pbItems];
+	return countOfItems > 0;
 }
 
-/* writeRowsWithIndexes
- * Use the common copy selection code to copy to
- * the pasteboard.
- */
--(BOOL)tableView:(NSTableView*)aListView writeRowsWithIndexes:(NSIndexSet*)rowIndexes toPasteboard:(NSPasteboard *)pboard
+// Provides a pasteboard writer for dragging article items
+- (id<NSPasteboardWriting>)tableView:(NSTableView *)tableView pasteboardWriterForRow:(NSInteger)row
 {
-	return [self copyTableSelection:rowIndexes toPasteboard:pboard];
+    return [self pasteboardWriterForIndex:row];
 }
 
 /* copy
